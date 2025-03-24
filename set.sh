35fe192cfa6b422f641f11cec1f6016debf4d638
nishra <nishrasshah@gmail.com>
2025-03-24 00:44:05 -0700

    added set.sh file

---
+++ set.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set laptop mobile tablet
+
+echo "laptop: $1"
+echo "mobile:$2"
+echo "tablet:$3"
+
+
+var=ajay
+echo Hello,$var
+
+a=10
+b=20
+ 
+if [ [ $a -gt 8 && $b -lt 15 ] ]
+then
+	echo"condition is true."
+else 
+	echo"condition is false."
+fi
+
+
+=======================================
+#this is my practise file 
+
